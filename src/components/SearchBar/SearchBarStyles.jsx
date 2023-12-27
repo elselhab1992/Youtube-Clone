a9bd9fc3cc558e9dd5ff1929a0429d85729812cc
Mohamed Elselhab <momanchester1992@gmail.com>
2023-12-27 12:29:42 +0000

    edit searchbar

--- src/components/SearchBar/SearchBarStyles.jsx
+++ src/components/SearchBar/SearchBarStyles.jsx
@@ -30,6 +30,7 @@ const Form = styled.form`
 
 const Input = styled.input`
   width: 90%;
+  height: 100%;
   border: none;
   outline: none;
   font-weight: ${(props) => props.theme.fontWeight.extraBold};
