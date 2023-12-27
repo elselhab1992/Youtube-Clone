484a7bc46f64427b5902dca254d9d75218e6d754
Mohamed Elselhab <momanchester1992@gmail.com>
2023-12-27 16:20:17 +0000

    create channel card component

--- src/components/ChannelCard/ChannelCard.jsx
+++ src/components/ChannelCard/ChannelCard.jsx
@@ -1,7 +1,37 @@
-import React from "react";
+/* eslint-disable react/prop-types */
+import { demoProfilePicture } from "../../utils/constants";
+import { Link } from "react-router-dom";
+import { ChannelLogo } from "./ChannelCardStyles";
+import { CheckCircle, ChannelName } from "../VideoCard/VideoCardStyles";
 
-const ChannelCard = () => {
-  return <div></div>;
+const ChannelCard = ({ details }) => {
+  console.log(details);
+
+  return (
+    <ChannelLogo>
+      <Link to={`/channel/${details?.id?.channelId}`}>
+        <img
+          src={details?.snippet?.thumbnails?.high?.url || demoProfilePicture}
+          alt={details?.snippet?.title}
+          width={250}
+          height={250}
+          style={{ borderRadius: "50%" }}
+        />
+      </Link>
+      <Link to={`/channel/${details?.id?.channelId}`}>
+        <ChannelName>
+          <h3 style={{ color: "grey" }}>{details?.snippet?.title}</h3>
+          <CheckCircle />
+        </ChannelName>
+      </Link>
+      {details?.statistics?.subscriberCount && (
+        <p>
+          {parseInt(details?.statistics?.subscriberCount).toLocaleString()}
+          Subscribers
+        </p>
+      )}
+    </ChannelLogo>
+  );
 };
 
 export default ChannelCard;
---
+++ src/components/ChannelCard/ChannelCardStyles.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const ChannelLogo = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+  margin: auto;
+`;
+
+export { ChannelLogo };
