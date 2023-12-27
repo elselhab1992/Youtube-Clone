b3454131c83163d872840c00152d9d2c73392d96
Mohamed Elselhab <momanchester1992@gmail.com>
2023-12-27 17:02:24 +0000

    channel details rrsponsive design

--- src/components/ChannelDetail/ChannelDetailStyles.jsx
+++ src/components/ChannelDetail/ChannelDetailStyles.jsx
@@ -2,6 +2,14 @@ import styled from "styled-components";
 
 const Section = styled.section`
   padding: 0 10rem;
+
+  @media (min-width: 300px) {
+    padding: 10px;
+  }
+
+  @media (min-width: 1200px) {
+    padding: 0 10rem;
+  }
 `;
 
 const ChannelLogo = styled.div`
