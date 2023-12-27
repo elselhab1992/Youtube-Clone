a13aec70c1ea21b6d0e7f26ef2d49cb67aca3ce3
Mohamed Elselhab <momanchester1992@gmail.com>
2023-12-27 17:09:10 +0000

    edit channel details

--- src/components/ChannelDetail/ChannelDetail.jsx
+++ src/components/ChannelDetail/ChannelDetail.jsx
@@ -2,7 +2,12 @@ import { fetchFromAPI } from "../../utils/fetchFromAPI";
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { Videos, ChannelCard } from "../index";
-import { ChannelLogo, ChannelVideos, Section } from "./ChannelDetailStyles";
+import {
+  ChannelBackground,
+  ChannelLogo,
+  ChannelVideos,
+  Section,
+} from "./ChannelDetailStyles";
 
 const ChannelDetail = () => {
   const [details, setDetails] = useState();
@@ -28,14 +33,7 @@ const ChannelDetail = () => {
   return (
     <Section>
       <div>
-        <div
-          style={{
-            height: "300px",
-            background:
-              "linear-gradient(90deg, rgba(0,238,247,1) 0%, rgba(206,3,184,1) 100%, rgba(0,212,255,1) 100%)",
-            zIndex: 10,
-          }}
-        />
+        <ChannelBackground />
         <ChannelLogo>
           <ChannelCard details={details} />
         </ChannelLogo>
--- src/components/ChannelDetail/ChannelDetailStyles.jsx
+++ src/components/ChannelDetail/ChannelDetailStyles.jsx
@@ -12,6 +12,30 @@ const Section = styled.section`
   }
 `;
 
+const ChannelBackground = styled.div`
+  height: 300px;
+
+  background: linear-gradient(
+    90deg,
+    rgba(0, 238, 247, 1) 0%,
+    rgba(206, 3, 184, 1) 100%,
+    rgba(0, 212, 255, 1) 100%
+  );
+  z-index: 10;
+
+  @media (min-width: 300px) {
+    height: 150px;
+  }
+
+  @media (min-width: 768px) {
+    height: 200px;
+  }
+
+  @media (min-width: 1200px) {
+    height: 300px;
+  }
+`;
+
 const ChannelLogo = styled.div`
   margin-top: -100px;
 `;
@@ -20,4 +44,4 @@ const ChannelVideos = styled.div`
   padding: 2rem 0;
 `;
 
-export { Section, ChannelLogo, ChannelVideos };
+export { Section, ChannelLogo, ChannelVideos, ChannelBackground };
